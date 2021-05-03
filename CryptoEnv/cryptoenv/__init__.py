34904881f6e79b04ba63d02f732aabb48a7fca83
StefB4 <StefB4>
2021-05-03 17:14:43 +0200

    reset

--- CryptoEnv/cryptoenv/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-from cryptoenv.envs.CryptoEnv import CryptoEnv
-
-register(
-    id='cryptoenv-v0',
-    entry_point='cryptoenv.envs:CryptoEnv',
-)
--- CryptoEnv/cryptoenv/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from cryptoenv.envs.CryptoEnv import CryptoEnv
